53baede0ce35ec2d1943949f4d60af94ca61ea1c
vinaneosoft <vinaneosoft@gmail.com>
2025-06-04 15:57:23 +0530

    commit

--- ObjectTypes.js
+++ ObjectTypes.js
@@ -33,3 +33,37 @@ console.log(direction1);
 var direction2 = Direction.UP;
 console.log(direction2);
 console.log(typeof direction1);
+// create enum Color : RED='hashcode', GREEN='hashcode', BLUE='hashcode'
+var arr1 = new Array(); // Array type
+arr1.push(56);
+arr1.push("hello");
+arr1.push(true);
+var arr2 = new Array(); // all values of type number
+arr2.push(56);
+//arr2.push("hello");
+var arr3 = new Array(); // all values of type string
+//arr3.push(56);
+arr3.push("hello");
+var arr4 = new Array(); // all values of type string or number
+arr4.push(88);
+arr4.push(56);
+arr4.push("hello");
+arr4.push("hi");
+arr4.push(12);
+var arr5 = new Array(); // all values of type string or number
+arr5.push(56);
+arr5.push("hello");
+var arr6 = new Array(); // all values of type string or number
+arr6.push(56);
+arr6.push("hello");
+// Tuples
+//let emps:[number, string]=['hari', 1234]
+var emps = [1234, 'hari'];
+emps[1] = 'pooja';
+console.log(emps);
+var emps2 = [1234, 'hari'];
+emps2[1] = 'pooja';
+console.log(emps2); // mutable
+var emps3 = [1234, 'hari'];
+//emps3[1]='pooja';   // immutable
+console.log(emps3);
--- ObjectTypes.ts
+++ ObjectTypes.ts
@@ -33,3 +33,52 @@ let direction2=Direction.UP;
 console.log(direction2);
 console.log(typeof direction1);
 
+// create enum Color : RED='hashcode', GREEN='hashcode', BLUE='hashcode'
+
+
+
+
+let arr1=new Array();  // Array type
+arr1.push(56);
+arr1.push("hello");
+arr1.push(true);
+
+let arr2:number[]=new Array();   // all values of type number
+arr2.push(56);
+//arr2.push("hello");
+
+let arr3=new Array<string>();   // all values of type string
+//arr3.push(56);
+arr3.push("hello");
+
+
+let arr4=new Array<string | number>();   // all values of type string or number
+arr4.push(88);
+arr4.push(56);
+arr4.push("hello");
+arr4.push("hi");
+arr4.push(12);
+
+let arr5:number[] | string[]=new Array(); // all values of type string or number
+(arr5 as number[]).push(56);
+(arr5 as string[]).push("hello");
+
+let arr6:(number|string)[]=new Array(); // all values of type string or number
+arr6.push(56);
+arr6.push("hello");
+
+
+// Tuples
+
+//let emps:[number, string]=['hari', 1234]
+let emps:[number, string]=[1234, 'hari']
+emps[1]='pooja'
+console.log(emps);
+
+let emps2:readonly [number, string]=[1234, 'hari']
+//emps2[1]='pooja'; // immutable
+console.log(emps2); 
+
+let emps3=[1234, 'hari'] as const
+//emps3[1]='pooja';   // immutable
+console.log(emps3);
