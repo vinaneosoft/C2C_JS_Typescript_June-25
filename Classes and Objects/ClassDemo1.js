a4401620122dc2d7c0ee2a99bf318916ea0f8d20
vinaneosoft <vinaneosoft@gmail.com>
2025-06-05 11:23:55 +0530

    commit

---
+++ Classes and Objects/ClassDemo1.js
@@ -0,0 +1,34 @@
+var BankAccount = /** @class */ (function () {
+    // default constructor
+    function BankAccount() {
+        console.log("called..");
+        this.accountNumber = 0;
+    }
+    // functions
+    BankAccount.prototype.checkBalance = function () {
+        return this.accountBalance;
+    };
+    BankAccount.prototype.deposit = function (deptamount) {
+        this.accountBalance = this.accountBalance + deptamount;
+        return this.accountBalance;
+    };
+    BankAccount.prototype.withdraw = function (withamount) {
+        // 0 balance allowed
+        if (this.accountBalance < withamount)
+            console.log("can not withdraw");
+        else
+            this.accountBalance = this.accountBalance - withamount;
+        return this.accountBalance;
+    };
+    return BankAccount;
+}());
+var account1 = new BankAccount(); // 0-p
+console.log(account1);
+console.log(account1.accountNumber);
+account1.accountNumber = 1; // state change in one object never affected other objects
+console.log(account1.accountNumber);
+var account2 = new BankAccount();
+console.log(account2);
+console.log(account2.accountNumber);
+account2.accountNumber = 2;
+console.log(account2.accountNumber);
--- Classes and Objects/ClassDemo1.ts
+++ Classes and Objects/ClassDemo1.ts
@@ -4,11 +4,21 @@ class BankAccount{
     customerName:string;
     accountBalance:number;
     accountType:string
+
+    // default constructor
+    constructor(){
+        console.log("called..");
+        this.accountNumber=0;
+        this.accountType='savings';
+        this.accountBalance=0;
+        this.customerId=0;
+        this.customerName='ABC'
+    }
+
     // functions
     checkBalance(){
         return this.accountBalance;
     }
-
     deposit(deptamount:number):number{
         this.accountBalance=this.accountBalance+deptamount;
         return this.accountBalance;
@@ -21,6 +31,19 @@ class BankAccount{
             this.accountBalance=this.accountBalance-withamount;
         return this.accountBalance
     }
-
 }
 
+const account1=new BankAccount(); // 0-p
+console.log(account1);
+console.log(account1.accountNumber);
+account1.accountNumber=1;   // state change in one object never affected other objects
+// change value for every property
+// again display ur account
+//console.log(account1.accountNumber);
+const account2=new BankAccount();
+console.log(account2);
+console.log(account2.accountNumber);
+account2.accountNumber=2;
+// change value for every property
+// again display ur account
+//console.log(account2.accountNumber);
